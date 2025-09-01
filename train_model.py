4aece1d1eeebe25522bc1f8c6af9000807b39792
JAAASN Yu <jaaasnyu@JAAASNs-MacBook-Pro.local>
2025-09-01 15:22:35 +0800

    Initial commit

--- train_model.py
+++ train_model.py
@@ -8,10 +8,12 @@ import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from gym.wrappers import GrayScaleObservation
 from test_obs import make_env
+from stable_baselines3.common.vec_env import SubprocessVecEnv
 
 #create environment for Mario
 def main():
     env = make_env()
+    vec_env = SubprocessVecEnv([make_env for _ in range(8)])
 
     model = PPO("CnnPolicy", env, verbose=1,tensorboard_log='logs',device='mps')
     model.learn(total_timesteps=1)
